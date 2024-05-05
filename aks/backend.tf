d136a9aa56f7a47361d0de406a2e0cf745c3628f
kollexy <59570875+kollexy@users.noreply.github.com>
2024-05-05 18:37:12 +0100

    Create backend.tf

---
+++ aks/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+#   backend "azurerm" {
+#     storage_account_name = "<storage_account_name>"
+#     container_name       = "<container_name>"
+#     key                  = "terraform.tfstate"  # The name of your state file
+#     access_key           = "<access_key>"
+#   }
+# }
