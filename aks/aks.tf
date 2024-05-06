302d9202bf5b93d77104aa1b7b8af95f81c8cd11
kollexy <59570875+kollexy@users.noreply.github.com>
2024-05-06 14:25:06 +0100

    change k8s version

--- aks/aks.tf
+++ aks/aks.tf
@@ -21,6 +21,7 @@ resource "azurerm_kubernetes_cluster" "k8s" {
   name                = var.aks_cluster_name
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = random_pet.azurerm_kubernetes_cluster_dns_prefix.id
+  kubernetes_version = "1.18"
 
   identity {
     type = "SystemAssigned"
