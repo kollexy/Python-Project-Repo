e77a5b3baa7f62637a9ca11ac2eccf09277cb065
kollexy <59570875+kollexy@users.noreply.github.com>
2024-05-07 07:54:23 +0100

    Update aks.tf

--- aks/aks.tf
+++ aks/aks.tf
@@ -1,16 +1,7 @@
-# Generate random resource group name
-# resource "random_pet" "rg_name" {
-#   prefix = var.resource_group_name_prefix
-# }
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
   name     = var.resource_group_name
 }
-
-# resource "random_pet" "azurerm_kubernetes_cluster_name" {
-#   prefix = "cluster"
-# }
 
 resource "random_pet" "azurerm_kubernetes_cluster_dns_prefix" {
   prefix = "dns"
